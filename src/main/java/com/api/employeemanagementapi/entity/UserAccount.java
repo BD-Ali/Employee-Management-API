2888f03edd70416289debfb9556dcbb54fe26b39
BD-Ali <badarne3li@gmail.com>
2025-09-30 21:06:56 +0300

    Added Auth

--- src/main/java/com/api/employeemanagementapi/entity/UserAccount.java
+++ src/main/java/com/api/employeemanagementapi/entity/UserAccount.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.apache.logging.log4j.util.Lazy;
 
 @Entity
 @Table (name = "user-account")
