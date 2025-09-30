d6e53131fb25a791cacde265036692ba53179964
BD-Ali <badarne3li@gmail.com>
2025-09-30 21:56:20 +0300

    Security

---
+++ src/main/java/com/api/employeemanagementapi/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.api.employeemanagementapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public org.springframework.security.web.SecurityFilterChain securityFilterChain(org.springframework.security.config.annotation.web.builders.HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/employees","/auth/signup").permitAll()
+
+                 );
+
+        return http.build();
+    }
+}
